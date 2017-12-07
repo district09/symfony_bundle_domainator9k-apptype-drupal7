3f75fd96bfe2361e1feeaf4eb00e1af71f365fbb
PMassoels <massoelspieter@gmail.com>
2017-12-07 09:08:09 +0100

    Drupal 7 entity added cleanup

---
+++ Entity/DrupalSevenApplication.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace DigipolisGent\Domainator9k\AppTypes\DrupalSevenBundle\Entity;
+
+use DigipolisGent\Domainator9k\CoreBundle\Entity\AbstractApplication;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+
+/**
+ * Class DrupalSevenApplication
+ * @package DigipolisGent\Domainator9k\AppTypes\DrupalSevenBundle
+ *
+ * @ORM\Entity()
+ */
+class DrupalSevenApplication extends AbstractApplication
+{
+
+    const TYPE = "DRUPAL_SEVEN";
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="3", max="255")
+     */
+    protected $installProfile;
+
+
+    public function getType()
+    {
+        return self::TYPE;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInstallProfile()
+    {
+        return $this->installProfile;
+    }
+
+    /**
+     * @param string $installProfile
+     */
+    public function setInstallProfile(string $installProfile)
+    {
+        $this->installProfile = $installProfile;
+    }
+}
