48458a2fbc732dc0d223c6ae4561786071c3dbf6
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2024-12-05 16:40:20 +0100

    Update to new directory structure for bundles

--- DigipolisGentDomainator9kAppTypesDrupalSevenBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace DigipolisGent\Domainator9k\AppTypes\DrupalSevenBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DigipolisGentDomainator9kAppTypesDrupalSevenBundle extends Bundle
-{
-}
